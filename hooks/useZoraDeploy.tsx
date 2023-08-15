70e2fe95a5099da7697dd3c457bbc61052f07d85
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-15 13:21:02 -0300

    I update useZoraHook to pass in animationUri of new znippet.

--- hooks/useZoraDeploy.tsx
+++ hooks/useZoraDeploy.tsx
@@ -6,17 +6,20 @@ import getZoraNFTCreatorProxyAddress from "../lib/getZoraNFTCreatorProxyAddress"
 import handleTxError from "../lib/handleTxError"
 import { uploadToIpfs } from "../lib/ipfs"
 import { useDeploy } from "../providers/DeployContext"
+import { clipZnippet } from "../lib/clipZnippet"
+import { bufferToWav } from "../lib/bufferToWav"
+import { blobToFile } from "../lib/blobToFile"
+import { uploadZnippetToIpfs } from "../lib/uploadZnippetToIpfs"
 
 const useZoraDeploy = () => {
   const signer = useEthersSigner()
   const { chain } = useNetwork()
   const { address } = useAccount()
-  const { audioFile } = useDeploy()
+  const { audioFile, wavesurfer, znippetStart, znippetEnd } = useDeploy()
 
   const createEditionWithReferral = async () => {
     try {
-      const cid = await uploadToIpfs(audioFile)
-      console.log("SWEETS CID", cid)
+      const cid = await uploadZnippetToIpfs(wavesurfer, znippetStart, znippetEnd, audioFile.type)
       const zoraNFTCreatorProxyAddres = getZoraNFTCreatorProxyAddress(chain?.id)
       const contract = new Contract(zoraNFTCreatorProxyAddres, abi, signer)
       const name = ""
@@ -35,7 +38,7 @@ const useZoraDeploy = () => {
         presaleMerkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
       }
       const description = ""
-      const animationUri = ""
+      const animationUri = `ipfs://${cid}`
       const imageUri = ""
       const createReferral = address
       const tx = await contract.createEditionWithReferral(
