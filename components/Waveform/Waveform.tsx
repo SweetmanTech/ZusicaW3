c102231f9e409f05c8650e19930012997d3091ee
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-15 12:29:41 -0300

    I update <Waveform /> to use useDeploy hook instead of useWaveform hook.

--- components/Waveform/Waveform.tsx
+++ components/Waveform/Waveform.tsx
@@ -1,9 +1,8 @@
-import { useCallback, useRef } from "react"
-import useWaveform from "../../hooks/useWaveform"
+import { useCallback } from "react"
+import { useDeploy } from "../../providers/DeployContext"
 
-const Waveform = (props) => {
-  const containerRef = useRef()
-  const { wavesurfer, regions, isPlaying, currentTime } = useWaveform(containerRef, props)
+const Waveform = () => {
+  const { isPlaying, currentTime, waveformRef, wavesurfer, regions } = useDeploy()
 
   const onPlayClick = useCallback(
     () => (wavesurfer.isPlaying() ? wavesurfer.pause() : regions.getRegions()[0].play()),
@@ -12,7 +11,7 @@ const Waveform = (props) => {
 
   return (
     <>
-      <div ref={containerRef} className="min-h-[120px] min-w-[50vw]" />
+      <div ref={waveformRef} className="min-h-[120px] min-w-[50vw]" />
 
       <button type="button" onClick={onPlayClick} style={{ marginTop: "1em" }}>
         {isPlaying ? "Pause" : "Play"}
