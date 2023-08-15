6aac40a30881e1a6d5661c7ac9fa82ff3c0083df
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-15 12:27:46 -0300

    I add useWaveform to DeployProvider.

--- providers/DeployContext.tsx
+++ providers/DeployContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useContext, useMemo } from "react"
+import { createContext, useState, useContext, useMemo, useRef } from "react"
+import useWaveform from "../hooks/useWaveform"
 
 const DeployContext = createContext({} as any)
 
@@ -7,10 +8,31 @@ export const useDeploy = () => useContext(DeployContext)
 export const DeployProvider = ({ children }) => {
   const [audioFile, setAudioFile] = useState(null)
   const [audioSrc, setAudioSrc] = useState(null)
+  const { isPlaying, currentTime, wavesurfer, regions, waveformRef } = useWaveform(audioSrc)
 
   const value = useMemo(
-    () => ({ audioFile, setAudioFile, audioSrc, setAudioSrc }),
-    [audioFile, setAudioFile, audioSrc, setAudioSrc],
+    () => ({
+      audioFile,
+      setAudioFile,
+      audioSrc,
+      setAudioSrc,
+      isPlaying,
+      currentTime,
+      wavesurfer,
+      regions,
+      waveformRef,
+    }),
+    [
+      audioFile,
+      setAudioFile,
+      audioSrc,
+      setAudioSrc,
+      isPlaying,
+      currentTime,
+      wavesurfer,
+      regions,
+      waveformRef,
+    ],
   )
 
   return <DeployContext.Provider value={value}>{children}</DeployContext.Provider>
