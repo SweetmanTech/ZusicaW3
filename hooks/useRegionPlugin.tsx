94d9682fcd1725d9600bc6d9c112176cab727558
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-15 17:17:43 -0300

    I add default region for fewer clicks.

--- hooks/useRegionPlugin.tsx
+++ hooks/useRegionPlugin.tsx
@@ -2,34 +2,36 @@ import { useEffect, useState } from "react"
 
 const useRegionPlugin = (ws) => {
   const [znippetStart, setZnippetStart] = useState(0)
-  const [znippetEnd, setZnippetEnd] = useState(0)
+  const [znippetEnd, setZnippetEnd] = useState(30)
   const [regionId, setRegionId] = useState(0)
 
   useEffect(() => {
     const init = async () => {
-      // MOVE THIS TO USEWAVEFORMREGION HOOK
       const MAX_LENGTH = 30
-      ws.enableDragSelection({
-        maxLength: MAX_LENGTH,
-        color: "rgb(255,255,255,0.7)",
-      })
 
       ws.on("region-update-end", (region: any) => {
         setZnippetStart(region.start)
         const tooLong = region.end - region.start > MAX_LENGTH
-        setZnippetEnd(tooLong ? region.start + 30 : region.end)
+        setZnippetEnd(tooLong ? region.start + MAX_LENGTH : region.end)
         setRegionId(region.id)
       })
 
       ws.on("region-out", (region) => {
         region.play()
       })
-      //   END REGION HOOK
+
+      const defaultRegion = ws.addRegion({
+        start: 0,
+        end: MAX_LENGTH,
+        color: "rgb(255,255,255,0.7)",
+      })
+      setRegionId(defaultRegion.id)
     }
 
     if (!ws) return
     init()
   }, [ws])
+
   return {
     znippetStart,
     znippetEnd,
